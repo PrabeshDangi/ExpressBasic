913cdb79bf88872ba42eccd2b4d034a93c72b868
Prabesh Dangi <dangiprabesh58@gmail.com>
2023-09-13 23:17:24 +0545

    created schema and model using express

--- server.js
+++ server.js
@@ -13,6 +13,37 @@ mongoose.connect(process.env.CONN_STR,{
 }).catch((err)=>{
     console.log("some error has occurred!!")
 })
+
+const movieSchema=new mongoose.Schema({
+    name: {
+        type:String,
+        required:[true,'The name is required!!'],
+        unique:true
+    },
+    description: {
+        type:String,
+        required:[true, 'The duration is required field!!']
+    },
+    duration: Number,
+    ratings: {
+        type:Number,
+        default:1.0
+    }
+});
+const Movie=mongoose.model('Movie',movieSchema)
+
+const testMovies=new Movie({
+    name:"Gangs of Wasseypur",
+    description:"The action movie packed with some chilling and OG Scenes.",
+    duration:260,
+    ratings:4.5
+})
+testMovies.save()
+.then((doc)=>{
+    console.log(doc);
+}).catch(err=>{
+    console.log("error has occurred!!")
+})
 //console.log(process.env)
 //Creating the server
 const port=process.env.PORT||5000;
