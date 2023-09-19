070c97c26a98fc1881c75fecbec75cb029b42258
Prabesh Dangi <dangiprabesh58@gmail.com>
2023-09-19 17:52:15 +0545

    Movie schema restructured

--- Models/movieModel.js
+++ Models/movieModel.js
@@ -4,16 +4,54 @@ const movieSchema=new mongoose.Schema({
     name: {
         type:String,
         required:[true,'The name is required!!'],
-        unique:true
+        unique:true,
+        trim:true
     },
     description: {
         type:String,
-        required:[true, 'The duration is required field!!']
+        required:[true, 'The duration is required field!!'],
+        trim:true
     },
-    duration: Number,
+    duration: {
+        type: Number,
+        required:[true,'The duration field is required!!']
+    },
     ratings: {
         type:Number,
-        default:1.0
+    },
+    totalRating:{
+        type:Number
+    },
+    releaseYear:{
+        type:Number,
+        required:[true,'The releaseYear field is required!!']
+    },
+    releaseDate:{
+        type:Date
+    },
+    createdAt:{
+        type:Date,
+        default: Date.now()
+    },
+    genres:{
+        type:[String],
+        required:[true, 'The generes field is required!!']
+    },
+    directors:{
+        type:[String],
+        required:[true, 'Directors field is required!!']
+    },
+    coverImage:{
+        type:[String],
+        required:[true,'Image is required field!!']
+    },
+    actors:{
+        type:[String],
+        required:[true,'Actors field is required!!']
+    },
+    price:{
+        type:Number,
+        required:[true, 'Price field is required!!']
     }
 });
 const Movie=mongoose.model('Movie',movieSchema)
